4d3011231e7d38158c79c8c68f4cea074ac6d81e
xxixx <terasys@naver.com>
2021-07-31 01:34:13 +0900

    install

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css' 
+import '@fortawesome/fontawesome-free/css/all.css' 
+import '@fortawesome/fontawesome-free/js/all.js' 
+
+createApp(App).mount('#app')
+//test//
